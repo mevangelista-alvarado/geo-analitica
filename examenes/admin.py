bac04c95eda340bf07b916a50e4b2853d97de5a3
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-05-18 19:43:12 -0500

    register to ExameneResuelto model to admin

--- examenes/admin.py
+++ examenes/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Examen
+from .models import Examen, ExamenResuelto
 
 admin.site.register(Examen)
+admin.site.register(ExamenResuelto)
